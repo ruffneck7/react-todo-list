468cf1ee885e7e7389e41f27eca944e977d7f0f3
ruffneck7(엄윤식) <ruffneck7@coupang.com>
2016-03-14 23:44:19 +0900

    step1

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+	render() {
+		return <div>Hello react app now?</div>
+	}
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: ['./client/client.js'],
+  output: {
+  	path: './dist',
+  	filename: 'bundle.js'
+  },
+  module: {
+  	loaders: [
+      {
+      	test: /\.js$/,
+      	loader: 'babel-loader',
+      	exclude: /node_modules/,
+      	query: {
+      	  presets: ['react', 'es2015']
+      	}
+      }
+  	]
+  }
+}
